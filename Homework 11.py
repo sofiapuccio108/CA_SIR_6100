3112a872466ac3693bdbddd3871dc02cb79c6216
sofiapuccio108 <sofiapuccio108@gmail.com>
2026-05-05 20:28:05 -0400

    edits to hw 11

---
+++ Homework 11.py
@@ -0,0 +1,10 @@
+### Homework 11: Structured Programming and CA Models ###
+# Sofia Puccio # 
+# 04/01/2026 # 
+
+# Listed below is the link to my new repository, in addition I have created a new file on my desktop called CA_SIR_6100 # 
+# This new file is organized with "functions", "figures", and "data" folders 
+
+https://github.com/sofiapuccio108/CA_SIR_6100.git
+
+# 
